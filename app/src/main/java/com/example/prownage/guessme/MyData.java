da3b43cb2881094cb66f96a75919520069b17183
Iodas <42702552+Iodas@users.noreply.github.com>
2018-09-26 22:30:47 +0200

    added new activity MyData

---
+++ app/src/main/java/com/example/prownage/guessme/MyData.java
@@ -0,0 +1,13 @@
+package com.example.prownage.guessme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_data);
+    }
+}
