a44bf9178ad0be936f5c353cd8628926aaf37477
Iodas <42702552+Iodas@users.noreply.github.com>
2018-09-27 15:43:05 +0200

    added 3 new Button intent to activities

--- app/src/main/java/com/example/prownage/guessme/MainActivity.java
+++ app/src/main/java/com/example/prownage/guessme/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.prownage.guessme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Button to get to the Activity MyData
+
+        Button MyDatabtn = (Button)findViewById(R.id.MyDataBtn);
+
+        MyDatabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MyData.class));
+            }
+        });
+
+
+        //Button to go to the Activity MyStatistics
+
+        Button MyStatisticsbtn = (Button)findViewById(R.id.MyStatisticsBtn);
+
+        MyStatisticsbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MyStatistics.class));
+            }
+        });
+
+
+        //Button to go to the Activity StartGuessing
+
+        Button StartGuessingbtn = (Button)findViewById(R.id.StartGuessingBtn);
+
+        StartGuessingbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StartGuessing.class));
+            }
+        });
+
+
     }
+
+
+
+
+
 }
